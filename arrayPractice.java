60b5c4da97ac177b807beb567dc69a2a92195931
Alex Dang <115202144+AuronAzryn@users.noreply.github.com>
2025-09-09 11:55:44 -0700

    Add files via upload

---
+++ arrayPractice.java
@@ -0,0 +1,24 @@
+
+public class arrayPractice {
+    public static void main(String[] args) {
+
+
+        // Create a string array using 4 elements
+        String[] myFourStrings = new String[4];
+
+        // Fill the array
+        myFourStrings[0] = "L";
+        myFourStrings[1] = "4";
+        myFourStrings[2] = "D";
+        myFourStrings[3] = "2";
+
+        // Output the array!
+        for (int i=0; i<4; i++) {
+            System.out.println("\n i is " + i + " and myFourStrings[" + i + "] is: "+ myFourStrings[i]);
+        }
+
+
+
+
+    }
+}
